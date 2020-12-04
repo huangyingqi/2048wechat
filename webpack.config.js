93c17e79cb6eddd8d88174c1f32a76474075a760
gg561 <tvgryphon2@gmail.com>
2020-12-04 17:48:43 -0500

    base ok for wechat

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,13 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const path = require("path");
-
 module.exports = env => {
     if (!env) {
         env = {};
     }
-    
     let config ={
-        // entry: "./src/ddz/game.ts",
-        // entry: "./src/game/huarongdao/game.ts",
-        // entry: "./src/game/index.ts",
         entry: "./src/game/2048/game.ts",
         output: {
-            // path: __dirname + "../2048",
-            // path: __dirname + "/games",//打包后的文件存放的地方
-            // filename: "./ddz.js"
-            // filename: "../games/huarongdao.js"
-            // filename: "../games/index.js"
             filename: "../../2048/game.js"
         },
-        // Enable sourcemaps for debugging webpack's output.
-        // devtool: "inline-source-map",
         resolve: {
             // Add '.ts' as resolvable extensions.
             extensions: [".ts", ".js"]
@@ -45,11 +31,5 @@ module.exports = env => {
 
         ]
     }  
-
-    if(!env.wx){
-        config.plugins.push(new HtmlWebpackPlugin())
-    }
-
-    return config
-    
-  }
+  return config
+}
